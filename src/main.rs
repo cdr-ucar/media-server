21ba9c1e4db28d9637438ffd575c086219e4c392
Emre Ucar <emre@cdrucar.fr>
2026-02-15 19:01:36 +0100

    feat: hello world + re-enable ctrlc

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,18 @@ use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
+    println!("Hello, world!");
+
     color_eyre::install()?;
 
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    // ctrlc::set_handler(|| {
-    //     tracing::info!("Received termination signal, exiting...");
-    //     std::process::exit(0);
-    // })?;
+    ctrlc::set_handler(|| {
+        tracing::info!("Received termination signal, exiting...");
+        std::process::exit(0);
+    })?;
 
     let config = config::AppConfig::load()?;
     let listen = config.listen.clone();
